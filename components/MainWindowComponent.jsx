73aee77857f64d3b68e05e7eb1d5ab05d668d790
sepehrfakour <sepehrfakour@users.noreply.github.com>
2017-01-01 03:46:54 -0800

    Add Footer

--- components/MainWindowComponent.jsx
+++ components/MainWindowComponent.jsx
@@ -1,6 +1,7 @@
 const React = require('react');
 
-const Header = require('./nav/HeaderComponent.jsx').default;
+const Header = require('./nav/HeaderComponent.jsx').default,
+      Footer = require('./nav/FooterComponent.jsx').default;
 
 var MainWindow = React.createClass({
   getInitialState: function () {
@@ -12,6 +13,7 @@ var MainWindow = React.createClass({
       <div id="main-window">
         <Header loggedIn={this.props.route.loggedIn} />
         {this.props.children}
+        <Footer />
       </div>
     );
   }
