9ecde5ed166f774a3c8ac3b01f088253c50d52c5
sepehrfakour <sepehrfakour@users.noreply.github.com>
2017-01-01 20:46:04 -0800

    Prep webpack for prod build for test deploy

--- webpack.config.js
+++ webpack.config.js
@@ -31,20 +31,20 @@ module.exports = {
 
   // To run on dev, switch devtools to source-map, comment out plugins, set output.publicPath url to webpack dev server URL
 
-  devtool: 'source-map',
-  // devtool: 'cheap-module-source-map',
+  // devtool: 'source-map',
+  devtool: 'cheap-module-source-map',
 
-  // plugins:[
-  //   new webpack.DefinePlugin({
-  //     'process.env':{
-  //       'NODE_ENV': JSON.stringify('production')
-  //     }
-  //   }),
-  //   new webpack.optimize.UglifyJsPlugin({
-  //     compress:{
-  //       warnings: true
-  //     }
-  //   })
-  // ],
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
 
 }
