94590773fba4d346005835b66bdcccdada71554f
sepehrfakour <sepehrfakour@users.noreply.github.com>
2017-01-03 18:58:39 -0800

    Basic Item Page layout

--- components/items/ItemPageComponent.jsx
+++ components/items/ItemPageComponent.jsx
@@ -22,11 +22,40 @@ var ItemPage = React.createClass({
   render: function () {
     return(
       <div id="item-page" className="container-fluid content">
-        <div className="banner">
-          <h1>Item Page</h1>
+        <div className="item-name">
+          <h2>{this.state.item.name}</h2>
         </div>
-        <div>{this.state.item.name}</div>
-        <div>${this.state.item.price}</div>
+        <div className="container item-container">
+          <div className="row">
+            <div className="offset-xs-1 col-xs-10 offset-md-0 col-md-6 border-box">
+              <div className="item-image">
+                <img src={this.state.item.imageurl}/>
+              </div>
+            </div>
+            <div className="offset-xs-1 col-xs-10 offset-md-0 col-md-6 border-box">
+              <div className="item-info">
+                <h4>{this.state.item.name}</h4>
+                <div>${this.state.item.price}</div>
+                <p>
+                  <label>Color</label>
+                  <input type="radio" name="color-radio"/>
+                  <input type="radio" name="color-radio"/>
+                  <input type="radio" name="color-radio"/>
+                </p>
+                <p>
+                  <label>Size</label>
+                  <select>
+                    <option>S</option>
+                    <option>M</option>
+                    <option>L</option>
+                  </select>
+                </p>
+                <button>Add to bag</button>
+                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   },
