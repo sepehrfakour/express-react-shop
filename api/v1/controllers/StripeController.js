bc77aa1fc276c2b46df2e2f75c872376e81a0b4f
sepehrfakour <sepehrfakour@users.noreply.github.com>
2017-01-25 21:02:36 -0800

    - Following a successful Stripe charge and Order Insert, update Stripe charge with order_id metadata - Improve server-side log messages

--- api/v1/controllers/StripeController.js
+++ api/v1/controllers/StripeController.js
@@ -5,6 +5,7 @@ const OrdersModel     = require('../models/OrdersModel.js'),
       OrderItemsModel = require('../models/OrderItemsModel.js');;
 
 class StripeController {
+
   constructor() {
     // super();
     this.preCharge        = this.preCharge.bind(this);
@@ -14,6 +15,11 @@ class StripeController {
     this.currentReq;
   }
   preCharge(req,res) {
+    /*
+    * Set instance variables and retreive items from DB in preparation for making a charge
+    * ------------------------------------------------------------------------------------
+    */
+    console.log("----------\nCharge request receieved");
     // TODO: closely double check this user input
     this.currentReq = {
       token: req.body.stripeToken,
@@ -31,12 +37,15 @@ class StripeController {
     this.currentReq.shipping.shipping_country = 'usa';
     this.currentReq.shipping.tax_rate = 0.08;
     this.currentReq.shipping.shipping_special_instructions = '';
-
     // Map cart to new array of IDs for querying
     let itemIds = this.currentReq.cart.map(function(item) { return item.id });
     ItemsModel.getItemsForOrder(itemIds,this.charge);
   }
   charge(result) {
+    /*
+    * Calculate amounts and create a new charge via Stripe API
+    * --------------------------------------------------------
+    */
     // First, get each queried item, find associated cart item by id, append order_quantity from cart item onto queried item
     let items = [];
     let that = this;
@@ -44,13 +53,14 @@ class StripeController {
     if (!Array.isArray(result)) {
       // In the case that preCharge db query returned single object, push it into an empty array
       // As long as queried item id matches cart item id, add order_quantity property
+      // Otherwise return a 400/error response
       if (result.id === that.currentReq.cart[0].id) {
         result.order_quantity = parseInt(that.currentReq.cart[0].quantity,10);
         items.push(result);
       } else { that.currentReq.res.status(400).end(); }
     } else {
-      // In the case that preCharge db query returned array of objects, map cart quantities into items array with nested loop
-      // TODO: Refactor nested loops
+      // In the case that preCharge db query returned array of objects, map cart quantities into items array with NESTED LOOP
+      // Consider revisiting this nested loop algo
       items = result.map( function (item) {
         // Initialize new order_quantity property on each queried item object
         item.order_quantity = 0;
@@ -63,11 +73,6 @@ class StripeController {
         return item;
       })
     }
-
-    // TODO: Retroactively add meta data to the Stripe charge object:
-    // metadata: {order_id: 6735},
-    // description: " list item names and quantities",
-
     // Initialize amount variables
     let subtotal = 0,
         tax = 0,
@@ -87,8 +92,8 @@ class StripeController {
     that.currentReq.payment.subtotal_amount = subtotal.toFixed(2);
     that.currentReq.payment.total_amount = total;
     that.currentReq.payment.tax_amount = tax.toFixed(2);
-    console.log("Payment amounts:",that.currentReq.payment);
-
+    console.log("Payment Total_Amount:",that.currentReq.payment.total_amount);
+    // Create Stripe charge
     let charge = stripe.charges.create({
       amount: amount,
       currency: that.currentReq.payment.currency,
@@ -101,12 +106,17 @@ class StripeController {
         that.currentReq.res.status(402).write(err.message);
         return that.currentReq.res.end();
       }
-      console.log("Stripe charge successful");
+      console.log("Stripe charge successful - CHARGE ID:",charge.id);
       that.currentReq.payment.status = 'charged';
       that.postCharge(charge);
     });
   }
+
   postCharge(charge) {
+    /*
+    * Create and insert a new order, then update the Stripe charge metadata with the order_id
+    * ---------------------------------------------------------------------------------------
+    */
     let that = this;
     let data = {
       status: that.currentReq.payment.status,
@@ -128,23 +138,43 @@ class StripeController {
       shipping_postal_code: that.currentReq.shipping.shipping_postal_code,
       shipping_special_instructions: that.currentReq.shipping.shipping_special_instructions
     };
-    OrdersModel.addOrder(data, function (result) {
-      console.log("Order insert successful");
-      // console.log("Order:",result);
-      // console.log("Charge:",charge);
-      that.insertOrderItems(result);
+    OrdersModel.addOrder(data, function (order) {
+      console.log("Order insert successful - ORDER ID:",order.id);
+      // Update the stripe charge with the new order ID
+      stripe.charges.update(
+        // TODO: Consider adding a description of the order here
+        charge.id,
+        {
+          metadata: { "order_id": order.id }
+        },
+        function (err, charge) {
+          // asynchronously called
+          if (err) {
+            console.log("Stripe update_charge error:",err);
+            that.currentReq.res.status(402).write(err.message);
+            return that.currentReq.res.end();
+          }
+          console.log("Stripe update_charge successful");
+          that.insertOrderItems(order);
+        }
+      );
     });
   }
+
   insertOrderItems(order) {
+    /*
+    * Create and insert new order_items for every item in the cart, then send 200/Success response to client
+    * ------------------------------------------------------------------------------------------------------
+    */
+    // Insert item_ids into order_items table, associating them with the order_id
     let that = this;
-    console.log("insertOrderItems ORDER:",order);
-    console.log("insertOrderItems IDS:",that.currentReq.cart);
     let data = {
       order_id: order.id,
       cart: that.currentReq.cart
     }
     console.log("insertOrderItems DATA:",data);
     OrderItemsModel.addOrderItems(data, function (result) {
+      console.log("Order_Items insert successful\n----------");
       that.currentReq.res.status(200);
       that.currentReq.res.end();
     })
