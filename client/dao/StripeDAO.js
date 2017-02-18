38f6e414e0fa52371e06383406f0796b30419323
sepehrfakour <sepehrfakour@users.noreply.github.com>
2017-02-18 06:36:23 -0800

    - Refactor all Data Access Objects; cleaner success/error handling - Refactor OrdersDAO to use Fetch polypill instead of XHR

--- client/dao/StripeDAO.js
+++ client/dao/StripeDAO.js
@@ -7,13 +7,13 @@ const CartActions = require('../actions/CartActions.js'),
 class StripeDAO {
   constructor() {
     this.stripeResponseHandler = this.stripeResponseHandler.bind(this);
-    this.errorBackToCart       = this.errorBackToCart.bind(this);
+    this._errorBackToCart      = this._errorBackToCart.bind(this);
   }
   createToken(payment) {
     let that = this;
     if (!window.Stripe) {
       console.warn('Stripe script not loaded');
-      that.errorBackToCart();
+      that._errorBackToCart();
     }
     else {
       // Get token
@@ -30,7 +30,7 @@ class StripeDAO {
     let that = this;
     if (response.error) {
       console.warn('Error creating Stripe token');
-      that.errorBackToCart(response.error.message);
+      that._errorBackToCart(response.error.message);
     }
     else {
       let token = response.id,
@@ -58,37 +58,34 @@ class StripeDAO {
       credentials: 'same-origin'
     });
 
-    fetch(request).then( function (res) {
-      return res.text().then(text => {
-        return {
-          message: text,
-          status: res.status
-        }
+    fetch(request)
+      .then( function (res) {
+        return res.text().then(text => {
+          return {
+            message: text,
+            status: res.status
+          }
+        });
+      })
+      .then( function(response) {
+        (response.status == 200) ? that._handleSuccess() : that._errorBackToCart(response.message,response.status);
       });
-      // return res.text();
-    }).then( function(response) {
-      let status = response.status;
-      if ((status !== 200)) {
-        that.errorBackToCart(status,response.message);
-      } else {
-        // Empty cart, navigate to home page, display success message
-        CartActions.clearCart();
-        browserHistory.push('/');
-        let msg = "Your order has been placed! You will receive an order confirmation email shortly.";
-        OverlayActions.setOverlay(false); // Deactivate loading overlay
-        AlertActions.addAlert(msg,'positive');
-        mixpanel.track('Payment Success');
-      }
-    });
   }
-  errorBackToCart(status,message) {
-    // Navigate to cart and display API/Stripe error message
-    browserHistory.push('/cart');
+  _handleSuccess() {
+    CartActions.clearCart(); // Empty cart
+    browserHistory.push('/'); // Navigate to home page
     OverlayActions.setOverlay(false); // Deactivate loading overlay
-    if (message && status !== 403) { // 403 implies invalid CSRF token, no need to display that error message
-      AlertActions.addAlert(message,'negative');
+    let msg = "Order received! You will get an order confirmation email shortly.";
+    AlertActions.addAlert(msg,'positive'); // Display success message
+    mixpanel.track('Payment Success'); // Log event
+  }
+  _errorBackToCart(message,status) {
+    browserHistory.push('/cart'); // Navigate to cart
+    OverlayActions.setOverlay(false); // Deactivate loading overlay
+    if (message && status !== 403) {
+      AlertActions.addAlert(message,'negative'); // Display API/Stripe error message
       mixpanel.track('Payment Failure', {message: message});
-    } else {
+    } else { // Either no message, or response status was a 403, implying Stripe script/token error, or invalid CSRF token
       AlertActions.addAlert("Payment could not be processed. Please try again later.",'negative');
       mixpanel.track('Payment Failure', {message: "No message."});
     }
